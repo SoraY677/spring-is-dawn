75709889b6f852bc002886c91871b6dad2b78b57
soray677 <soray677@outlook.com>
2023-05-05 13:16:03 +0900

    init

---
+++ src/main/kotlin/com/spring/akebono/AkebonoApplication.kt
@@ -0,0 +1,11 @@
+package com.spring.akebono
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AkebonoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AkebonoApplication>(*args)
+}
