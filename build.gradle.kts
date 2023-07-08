e80a120757a1f336783ceb2abef9ca28a170fa50
soray677 <soray677@outlook.com>
2023-07-08 12:02:35 +0900

    API読み込みを検証

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("com.fasterxml.jackson.core:jackson-databind")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- src/main/kotlin/com/spring/akebono/Controller/Index.kt
+++ src/main/kotlin/com/spring/akebono/Controller/Index.kt
@@ -1,16 +1,25 @@
 package com.spring.akebono.Controller
 
+import java.net.URL;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model
-import com.spring.akebono.Util.PathUtil
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.spring.akebono.Data.ApiData
 
 @Controller
 class HelloController {
 	@RequestMapping(value = arrayOf("/"))
 	fun index(model: Model):String {
-		model.addAttribute("PathUtil", PathUtil())
+		val mapper = ObjectMapper();
+
+		val response = mapper.readValue<ApiData>(
+			URL("http://localhost:8080/api/"),
+			ApiData::class.java
+		)
+
+		model.addAttribute("jsonData", response)
 		return "index"
 	}
 }
